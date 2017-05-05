bab3799f9394b4e47130e21fbdd3830e13f0d20f
Timm Bäder <mail@baedert.org>
2017-05-05 18:24:52 +0200

    widget: Add private get_margin_allocation

--- gtk/gtkwidgetprivate.h
+++ gtk/gtkwidgetprivate.h
@@ -318,6 +318,9 @@ void              gtk_widget_get_content_allocation        (GtkWidget        *wi
                                                             GtkAllocation    *allocation);
 void              gtk_widget_get_border_allocation         (GtkWidget        *widget,
                                                             GtkAllocation    *allocation);
+void              gtk_widget_get_margin_allocation         (GtkWidget        *widget,
+                                                            GtkAllocation    *allocation);
+
 
 /* inline getters */
 
